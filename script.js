c21b379881819e5494e1aa23d2803bdf064caddd
Kapil Dagar <116935052+happydagar6@users.noreply.github.com>
2026-02-14 10:55:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let timerDisplay = document.getElementById("timer");
+
+// Set current date and time
+let currentDate = new Date();
+
+// Update the time 
+setInterval(() => {
+	currentDate = new Date();
+	timerDisplay.innerHTML = currentDate.toLocaleString();
+}, 1000)
